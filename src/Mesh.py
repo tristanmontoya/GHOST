b84b859d9ef0546a1583989e91f1b6fd0d0d82a8
tristanmontoya <tristan.montoya@mail.utoronto.ca>
2020-10-17 23:56:08 -0400

    periodicity along coordinate axes works

--- src/Mesh.py
+++ src/Mesh.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from abc import ABC, abstractmethod
+from functools import partial
 import meshio
 
 BC_TOL = 1.0e-8
@@ -19,10 +20,6 @@ class Mesh(ABC):
         self.compute_affine_mapping()
         self.map_mesh()
         
-        # initialize bc list as empty list, will contain functions with
-        # boundary conditions as function of x (restricted to boundary) and t
-        self.bcs = []
-    
     @abstractmethod 
     def compute_affine_mapping(self):
         pass
@@ -32,7 +29,6 @@ class Mesh(ABC):
     def plot_mesh(self, fontsize=8):
         pass
     
-    
     def map_mesh(self, f_map=lambda x: x, J_map = None):
         
         # default is to keep original affine mapping, do not curve
@@ -41,43 +37,93 @@ class Mesh(ABC):
         
         self.X = [(lambda xi, k=k: f_map(self.X_affine[k](xi))) 
                   for k in range(0,self.K)]
-        self.J = [(lambda xi, k=k: J_map(self.X_affine[k](xi)) @ self.J_affine[k](xi)) 
+        self.J = [(lambda xi, k=k: 
+                   J_map(self.X_affine[k](xi)) @ self.J_affine[k](xi))
                   for k in range(0,self.K)]
         self.detJ = [(lambda xi, k=k: np.linalg.det(self.J(xi))) 
                      for k in range(0,self.K)]
     
     
-    def add_bc_at_facet(self, local_index, bc_function):
+    def add_bc_at_facet(self, local_index, bc_index):
         
-        self.bcs.append(bc_function)
-        self.local_to_bc_index[local_index] = len(self.bcs)
+        self.local_to_bc_index[local_index] = bc_index
         
         
-    def add_bc_by_indicator(self, indicator, bc_function):
+    def add_bcs_by_indicators(self, indicator, bc_index):
         
         # indicator function is defined such that it is zero everywhere except
-        # for the boundary where this bc is being imposed (lines/faces should have
-        # thickness for tolerance)
+        # for the boundary where this bc is being imposed (lines/faces 
+        # should have thickness for tolerance)
         
-        # have to loop through all facets and see which ones are on this boundary
+        # have to loop through all facets and see which ones have midpoints 
+        # on which indicated boundary
         for k in range(0,self.K):
             for gamma in range(0,self.Nf_local[k]): 
-                if indicator(self.v[self.local_to_vertex((k,gamma))]) != 0:
-                    self.add_bc_at_facet((k,gamma), bc_function)
+                
+                # get physical vertex locations for this local facet
+                facet_vertices = [self.v[self.local_to_vertex[k][gamma][i]] 
+                                  for i in range(0,len(
+                                          self.local_to_vertex[k][gamma]))]
+               
+                # add bc if lies on midpoint of facet
+                midpoint = sum(facet_vertices)/len(facet_vertices)
+                for i in range(0,len(indicator)):
+                    if indicator[i](midpoint) != 0:
+                        self.add_bc_at_facet((k,gamma), bc_index[i])
 
-    def add_bc_on_hyperplane(self, coeffs, bc_function, tol=BC_TOL):
-        pass
-    
-    
-    def hyperplane_indicator(self, coeffs, tol=BC_TOL):
+
+    def add_bc_on_hyperplanes(self, coeffs, bc_index, tol=BC_TOL):
         
-        def hyperplane(x):
-            if np.dot(coeffs[0:self.d], x) - coeffs[self.d-1] < tol:
-                return 1
-            else:
-                return 0
+        hyperplanes = [partial(Mesh.hyperplane_indicator,coeffs[i],tol) 
+                       for i in range(0,len(coeffs))]
         
-        return hyperplane
+        self.add_bcs_by_indicators(hyperplanes, bc_index)
+        
+        
+    def make_periodic(self, bc_index, hyperplane_axes=[0], tol=BC_TOL):
+        
+        # use local-to-local conectivity to model periodicity
+        
+        for local_index in self.local_to_bc_index:
+
+            if self.local_to_bc_index[local_index] == bc_index[0]:
+                k,gamma = local_index[0], local_index[1]
+                
+                # get physical vertex locations for this local facet
+                facet_vertices = [self.v[self.local_to_vertex[k][gamma][i]]
+                                  for i in range(0,len(
+                                          self.local_to_vertex[k][gamma]))]
+                
+                midpoint = sum(facet_vertices)/len(facet_vertices)
+                
+                for other_index in self.local_to_bc_index:
+                    if self.local_to_bc_index[other_index] == bc_index[1]:
+                        nu,rho = other_index[0], other_index[1]
+                        
+                        other_facet_vertices = [self.v[self.local_to_vertex[nu][rho][i]]
+                                      for i in range(0,len(
+                                              self.local_to_vertex[nu][rho]))]
+                        other_midpoint = sum(other_facet_vertices)/len(
+                            other_facet_vertices)   
+                        
+                        # match up if corresponds to translation along a coordinate axis
+                        if max([midpoint[hyperplane_axes[axis]] - other_midpoint[hyperplane_axes[axis]] 
+                                for axis in range(0,len(hyperplane_axes))]) < BC_TOL:
+                            
+                            
+                            # add local-to-local connectivity to dictionary
+                            self.local_to_local[k,gamma] = (nu,rho)
+                            self.local_to_local[nu,rho] = (k,gamma)             
+                            break
+                    
+        
+    @staticmethod
+    def hyperplane_indicator(coeffs, tol, x):
+        if np.abs(np.dot(coeffs[0:-1], x) - coeffs[-1]) < tol:
+            return 1
+        else:
+            return 0
+        
         
 class Mesh1D(Mesh):
     
@@ -86,8 +132,11 @@ class Mesh1D(Mesh):
                  periodic=True,
                  transform=None):
         
-        # number of elements
         self.K = K
+        self.Nv_global = self.K+1
+        self.Nv_local = [2 for k in range(0,self.K)]
+        self.Nf_local = self.Nv_local.copy()
+        
 
         # generate vertices
         if spacing == 'uniform':
@@ -96,31 +145,48 @@ class Mesh1D(Mesh):
             raise NotImplementedError
             
         # generate element to vertex connectivity
-        self.Nv_local = [2 for k in range(0,self.K)]
-        self.Nv_global = self.K+1
+        self.element_to_vertex = [[k,k+1] for k in range(0,self.K)]
         
         # generate local facet to global vertex connectivity
-        self.local_to_vertex = [[[k],[k+1]] for k in range(0,self.K)]
+        self.local_to_vertex = [[(k,),(k+1,)] for k in range(0,self.K)]
         
-        # generate global to local facet connectivity
-        self.Nf_local = [2 for k in range(0,self.K)]
+        # local to local is None for boundary facets
+        self.local_to_local = {}
+        
+        # when evaluating fluxes look for this when local to local is None
+        self.local_to_bc_index = {}
         
         if periodic:
-            self.global_to_local = [[(k,1),(k+1,0)] for k in range(0,self.K-1)]
-            self.global_to_local.insert(0,[(self.K-1,1),(0,0)])
-            self.Nf_global = self.K
+            global_to_local = [[(k,1),(k+1,0)] for k in range(0,self.K-1)]
+            global_to_local.insert(0,[(self.K-1,1),(0,0)])
             
         else:
-            self.global_to_local = [[(k,1),(k+1,0)] for k in range(0,self.K)]
-            self.Nf_global = self.K+1
+           global_to_local = [[(k,1),(k+1,0)] for k in range(0,self.K)]
     
         # generate local to local connectivity (kappa,gamma to rho,nu)
-        self.local_to_local = {}
-        for f in range(0,self.Nf_global):
-            self.local_to_local[self.global_to_local[f][0][0],
-                                self.global_to_local[f][0][1]] = self.global_to_local[f][1]
-            self.local_to_local[self.global_to_local[f][1][0],
-                                self.global_to_local[f][1][1]] = self.global_to_local[f][0]
+
+        # get interior facet to facet connectivity
+        for k in range(0,self.K):
+            
+            for gamma in range(0,2):
+                
+                # initially assume face has no neignbours before searching
+                self.local_to_local[k,gamma] = None
+                
+                # find (nu,rho) matching (k,gamma)
+                for nu in range(0,self.K):
+                    if nu == k:
+                        continue
+                    try:
+                        rho = self.local_to_vertex[nu].index(
+                            (self.local_to_vertex[k][gamma][0],))
+                    except ValueError:
+                        # this element is not a neighbour of (k,gamma)
+                        continue 
+    
+                    # add to dictionaries
+                    self.local_to_local[k,gamma] = (nu,rho)
+                    break
     
         super().__init__(name,1)
         
@@ -128,15 +194,17 @@ class Mesh1D(Mesh):
     def compute_affine_mapping(self):
         
         # map from (-1, 1) to physical element
-        self.X_affine = [(lambda xi, k=k: self.v[self.local_to_vertex[k][0][0]] \
-                          + 0.5*(self.v[self.local_to_vertex[k][1][0]] \
-                                -self.v[self.local_to_vertex[k][0][0]])*(xi+1)) \
-                             for k in range(0,self.K)]
+        self.X_affine = [(lambda xi, k=k: self.v[
+            self.local_to_vertex[k][0][0]] + 0.5*(
+                self.v[self.local_to_vertex[k][1][0]] -self.v[
+                                  self.local_to_vertex[k][0][0]])*(xi+1)) 
+                         for k in range(0,self.K)]
         
         self.J_affine = [(lambda xi,k=k: 
-                          np.array([0.5*(self.v[self.local_to_vertex[k][1][0]] \
-                                                    -self.v[self.local_to_vertex[k][0][0]])])) \
-                                                     for k in range(0,self.K)]
+                          np.array([0.5*(
+                              self.v[self.local_to_vertex[k][1][0]] -self.v[
+                                  self.local_to_vertex[k][0][0]])]))
+                         for k in range(0,self.K)]
     
         
     def plot_mesh(self, fontsize=8):
@@ -185,13 +253,16 @@ class Mesh2D(Mesh):
         # get facet to vertex connectivity
         self.local_to_vertex = [[(self.element_to_vertex[k][i],
                                   self.element_to_vertex[k][i+1]) 
-                                 for i in range(0,self.Nv_local[k]-1)] \
-                                + [(
-                                    self.element_to_vertex[k][self.Nv_local[k]-1],
+                                 for i in range(0,self.Nv_local[k]-1)] + [(
+                                    self.element_to_vertex[k][
+                                        self.Nv_local[k]-1],
                                     self.element_to_vertex[k][0])] 
                                 for k in range(0,self.K)]
         
+        # local to local is None for boundary facets
         self.local_to_local = {}
+        
+        # when evaluating fluxes look for this when local to local is None
         self.local_to_bc_index = {}
         
         # get interior facet to facet connectivity
@@ -200,9 +271,6 @@ class Mesh2D(Mesh):
                 
                 # initially assume face has no neignbours before searching
                 self.local_to_local[k,gamma] = None
-                
-                # (boundary index of 0 means not a boundary facet)
-                self.local_to_bc_index[k,gamma] = 0
                 
                 # find (nu,rho) with edge containing matching vertices 
                 # corresponding to (k,gamma) swapped (due to CCW ordering)
@@ -216,6 +284,7 @@ class Mesh2D(Mesh):
                         
                     # add to dictionaries
                     self.local_to_local[k,gamma] = (nu,rho)
+                    break
                 
         super().__init__(name,2)
         
@@ -244,5 +313,6 @@ class Mesh2D(Mesh):
         ax.get_yaxis().set_visible(False) 
         plt.axis('off')
         plt.show()
-        meshplt.savefig("../plots/" + self.name + ".pdf", bbox_inches=0, pad_inches=0)
+        meshplt.savefig("../plots/" + self.name + ".pdf",
+                        bbox_inches=0, pad_inches=0)
     
