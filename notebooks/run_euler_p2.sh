59612c4006afeec411a01a89fb19bf4c7ca7e1d4
Tristan Montoya <montoya.tristan@gmail.com>
2021-10-22 15:32:05 -0400

    made p_geo to p_map

--- notebooks/run_euler_p2.sh
+++ notebooks/run_euler_p2.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 # c = c_dg
-papermill euler_driver.ipynb euler_m04p2c0t1_strong.ipynb -p mach_number 0.4 -p p 2 -p p_geo 2 -p c 'c_dg' -p discretization_type 1 -p form 'strong' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2c0t1_strong.out &
+papermill euler_driver.ipynb euler_m04p2c0t1_strong.ipynb -p mach_number 0.4 -p p 2 -p p_map 2 -p c 'c_dg' -p discretization_type 1 -p form 'strong' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2c0t1_strong.out &
 
-papermill euler_driver.ipynb euler_m04p2c0t1_weak.ipynb -p mach_number 0.4 -p p 2 -p p_geo 2 -p c 'c_dg' -p discretization_type 1 -p form 'weak' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2c0t1_weak.out &
+papermill euler_driver.ipynb euler_m04p2c0t1_weak.ipynb -p mach_number 0.4 -p p 2 -p p_map 2 -p c 'c_dg' -p discretization_type 1 -p form 'weak' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2c0t1_weak.out &
 
-papermill euler_driver.ipynb euler_m04p2c0t2_strong.ipynb -p mach_number 0.4 -p p 2 -p p_geo 2 -p c 'c_dg' -p discretization_type 2 -p form 'strong' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2c0t2_strong.out &
+papermill euler_driver.ipynb euler_m04p2c0t2_strong.ipynb -p mach_number 0.4 -p p 2 -p p_map 2 -p c 'c_dg' -p discretization_type 2 -p form 'strong' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2c0t2_strong.out &
 
-papermill euler_driver.ipynb euler_m04p2c0t2_weak.ipynb -p mach_number 0.4 -p p 2 -p p_geo 2 -p c 'c_dg' -p discretization_type 2 -p form 'weak' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2c0t2_weak.out &
+papermill euler_driver.ipynb euler_m04p2c0t2_weak.ipynb -p mach_number 0.4 -p p 2 -p p_map 2 -p c 'c_dg' -p discretization_type 2 -p form 'weak' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2c0t2_weak.out &
 
-papermill euler_driver.ipynb euler_m04p2c0t3_strong.ipynb -p mach_number 0.4 -p p 2 -p p_geo 2 -p c 'c_dg' -p discretization_type 3 -p form 'strong' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2c0t3_strong.out &
+papermill euler_driver.ipynb euler_m04p2c0t3_strong.ipynb -p mach_number 0.4 -p p 2 -p p_map 2 -p c 'c_dg' -p discretization_type 3 -p form 'strong' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2c0t3_strong.out &
 
-papermill euler_driver.ipynb euler_m04p2c0t3_weak.ipynb -p mach_number 0.4 -p p 2 -p p_geo 2 -p c 'c_dg' -p discretization_type 3 -p form 'weak' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2c0t3_weak.out &
+papermill euler_driver.ipynb euler_m04p2c0t3_weak.ipynb -p mach_number 0.4 -p p 2 -p p_map 2 -p c 'c_dg' -p discretization_type 3 -p form 'weak' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2c0t3_weak.out &
 
 # c = c_+
-papermill euler_driver.ipynb euler_m04p2cpt1_strong.ipynb -p mach_number 0.4 -p p 2 -p p_geo 2 -p c 'c_+' -p discretization_type 1 -p form 'strong' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2cpt1_strong.out &
+papermill euler_driver.ipynb euler_m04p2cpt1_strong.ipynb -p mach_number 0.4 -p p 2 -p p_map 2 -p c 'c_+' -p discretization_type 1 -p form 'strong' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2cpt1_strong.out &
 
-papermill euler_driver.ipynb euler_m04p2cpt1_weak.ipynb -p mach_number 0.4 -p p 2 -p p_geo 2 -p c 'c_+' -p discretization_type 1 -p form 'weak' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2cpt1_weak.out &
+papermill euler_driver.ipynb euler_m04p2cpt1_weak.ipynb -p mach_number 0.4 -p p 2 -p p_map 2 -p c 'c_+' -p discretization_type 1 -p form 'weak' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2cpt1_weak.out &
 
-papermill euler_driver.ipynb euler_m04p2cpt2_strong.ipynb -p mach_number 0.4 -p p 2 -p p_geo 2 -p c 'c_+' -p discretization_type 2 -p form 'strong' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2cpt2_strong.out &
+papermill euler_driver.ipynb euler_m04p2cpt2_strong.ipynb -p mach_number 0.4 -p p 2 -p p_map 2 -p c 'c_+' -p discretization_type 2 -p form 'strong' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2cpt2_strong.out &
 
-papermill euler_driver.ipynb euler_m04p2cpt2_weak.ipynb -p mach_number 0.4 -p p 2 -p p_geo 2 -p c 'c_+' -p discretization_type 2 -p form 'weak' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2cpt2_weak.out &
+papermill euler_driver.ipynb euler_m04p2cpt2_weak.ipynb -p mach_number 0.4 -p p 2 -p p_map 2 -p c 'c_+' -p discretization_type 2 -p form 'weak' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2cpt2_weak.out &
 
-papermill euler_driver.ipynb euler_m04p2cpt3_strong.ipynb -p mach_number 0.4 -p p 2 -p p_geo 2 -p c 'c_+' -p discretization_type 3 -p form 'strong' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2cpt3_strong.out &
+papermill euler_driver.ipynb euler_m04p2cpt3_strong.ipynb -p mach_number 0.4 -p p 2 -p p_map 2 -p c 'c_+' -p discretization_type 3 -p form 'strong' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2cpt3_strong.out &
 
-papermill euler_driver.ipynb euler_m04p2cpt3_weak.ipynb -p mach_number 0.4 -p p 2 -p p_geo 2 -p c 'c_+' -p discretization_type 3 -p form 'weak' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2cpt3_weak.out &
+papermill euler_driver.ipynb euler_m04p2cpt3_weak.ipynb -p mach_number 0.4 -p p 2 -p p_map 2 -p c 'c_+' -p discretization_type 3 -p form 'weak' &>/scratch/z/zingg/tmontoya/tmp/papermill_m04p2cpt3_weak.out &
 
 wait
